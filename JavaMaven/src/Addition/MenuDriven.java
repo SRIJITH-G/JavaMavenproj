7888bf44b4540f83cff68052daf423d9d93ee9a9
SRIJITH-G <srijithgovindarajan@gmail.com>
2020-08-18 14:51:51 +0530

    mavenCommit

--- JavaMaven/src/Addition/MenuDriven.java
+++ JavaMaven/src/Addition/MenuDriven.java
@@ -15,7 +15,7 @@ package Addition;
 			
 			System.out.println("Only one dish for one person");
 			System.out.println("Enter c for Chapathi - 40Rs. ");
-			System.out.println("Enter f for fried Rice - 90Rs.");
+			System.out.println("Enter f for Shawarma - 90Rs.");
 			System.out.println("Enter p for Poori - 55Rs.");
 			System.out.println("Enter d for Dosa -75Rs.");
 			String s = input.next();
